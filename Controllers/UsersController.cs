209821eb0c39b1c53d1a8152a1fbd18daf3c294e
Yorgo Custers <417305@student.fontys.nl>
2022-05-23 15:28:41 +0200

    dependency injection fixed

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,6 @@
 using System;
+using AutoMapper;
+using lidl_twitter_tweet_service.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lidl_twitter_tweet_service.Controllers
@@ -7,10 +9,13 @@ namespace lidl_twitter_tweet_service.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly ILidlTweetRepo _repository;
+        private readonly IMapper _mapper;
 
-        public UsersController()
+        public UsersController(ILidlTweetRepo repository, IMapper mapper)
         {
-            
+            _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpPost]
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using lidl_twitter_tweet_service.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,8 @@ namespace lidl_twitter_tweet_service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemoryDB"));
+            services.AddScoped<ILidlTweetRepo, LidlTweetRepo>();
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
